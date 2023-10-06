87f94f099d60106faf35b0a5c81581b683b56bb7
bitcode001 <82773560+bitcode001@users.noreply.github.com>
2023-10-06 23:08:06 +0100

    feat(): added new seo astro pack and added robots.txt with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import partytown from '@astrojs/partytown'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		react(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push']
-			}
-		})
-	]
-})
+	site: 'https://www.anilrai.dev',
+  	integrations: [tailwind(), react(), partytown({
+		config: {
+		forward: ['dataLayer.push']
+		}
+	}), sitemap()]
+});
