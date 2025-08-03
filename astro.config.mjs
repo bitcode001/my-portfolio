245ea8d55246af781806167be284edc0fda11a33
Anil Rai <82773560+bitcode001@users.noreply.github.com>
2025-08-04 00:16:43 +0100

    Feat/blog (#4)
    * Basic building block for Blogs in Site
    
    * First working layout of blog page and started first content
    
    * Completed first blog and launching blog section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,40 @@ import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
 import partytown from '@astrojs/partytown';
 
+import mdx from '@astrojs/mdx';
+import rehypeSlug from 'rehype-slug';
+import rehypePrettyCode from 'rehype-pretty-code';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.anilrai.dev',
-  	integrations: [tailwind(), react(), partytown({
-		config: {
-			forward: ['dataLayer.push']
-		}
-	}), sitemap()]
+    site: 'https://www.anilrai.dev',
+  	integrations: [
+        tailwind(), react(), 
+        partytown({
+            config: {
+                forward: ['dataLayer.push']
+            }
+        }), 
+        sitemap(), 
+        mdx({
+            syntaxHighlight: false,
+            rehypePlugins: [
+                /**
+                * Adds ids to headings
+                */
+                rehypeSlug,
+                [
+                /**
+                * Enhances code blocks with syntax highlighting, line numbers,
+                * titles, and allows highlighting specific lines and words
+                */
+        
+                rehypePrettyCode,
+                {
+                    theme: 'github-dark',
+                },
+                ],
+            ],
+        })
+    ]
 });
